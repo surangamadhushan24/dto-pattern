4269555c6bb1cb2e50199fa98d40055c45b63d51
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-06-17 21:34:19 +0530

    first commit

---
+++ src/test/java/com/example/dtodemo/DtoPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dtodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
